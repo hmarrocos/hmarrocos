8c0d71bd61745a5b1f8298ac5452cdef662b635f
Marrocos <tendhendriw_94@hotmail.com>
2022-07-20 21:59:57 -0300

    adj 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,12 +26,12 @@ export default function Home(): JSX.Element {
 
         <div className={styles.grid}>
           <a className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2>Documentation</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a className={styles.card}>
-            <h2>Learn &rarr;</h2>
+            <h2>Learn</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
         </div>
