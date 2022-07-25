08cd0c260744d3bff4627028b573e6e631269c44
Marrocos <tendhendriw_94@hotmail.com>
2022-07-25 16:11:20 -0300

    g to ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,107 +1,141 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
-import Header from './header'
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import Header from "./header";
 
-import { Disclosure } from '@headlessui/react'
-import { ChevronUpIcon } from '@heroicons/react/solid'
+import { Disclosure } from "@headlessui/react";
+import { ChevronUpIcon } from "@heroicons/react/solid";
 
 export default function Home(): JSX.Element {
   return (
     <>
-    <Header />
-    <div className={styles.container}>
-      <Head>
-        <title>Marrocos</title>
-        <meta name="description" content="Powered by Marrocos" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Header />
+      <div className={styles.container}>
+        <Head>
+          <title>Marrocos</title>
+          <meta name="description" content="Powered by Marrocos" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hendriw Marrocos
-        </h1>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Hendriw Marrocos</h1>
 
-        <p className={styles.description}>
-        Desenvolvedor Web Front-end
-        </p>
+          <p className={styles.description}>Desenvolvedor Web Front-end</p>
 
-        <div className={styles.grid}>
-          <a className={styles.card}>
-            <h2>Olá!</h2>
-            <p>
-              Sou Desenvolvedor Web de{' '}
-              <a 
-              href="https://google.com/maps/place/Sao+Paulo" 
-              className={styles.code}>
-                São Paulo, Brasil
-              </a>
-              , e amo o que faço.
-            </p>
-          </a>
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              <h2>Olá!</h2>
+              <p>
+                Sou Desenvolvedor Web de{" "}
+                <a
+                  href="https://google.com/maps/place/Sao+Paulo"
+                  className={styles.code}
+                >
+                  São Paulo, Brasil
+                </a>
+                , e amo o que faço.
+              </p>
+              <div className={styles.buttons}>
+                <button>Sobre mim</button>
+                <button>Competências</button>
+                <button>Projetos</button>
+                <button>Contato</button>
+              </div>
+            </div>
 
-          <a className={styles.card}>
-            <h2>Sobre mim</h2>
-            <p>Tenho 21 anos de idade.</p>
-            <p>Gosto de conhecer pessoas novas, viajar, natureza e explorar o desconhecido.</p>
-            <p>Desde criança lido com tecnologia, sempre buscando por solucionar problemas ou aprender algo novo.</p>
-            <p>Programo há 5 anos, porém na área há 6 meses.</p>
-          </a>
-        </div>
+            <div className={styles.card}>
+              <h2>Sobre mim</h2>
+              <p>
+                Técnico em Informática e aluno de graduação, com habilidades em
+                resolução de problemas.
+              </p>
+              <p>
+                Busco estagiar por meio da criatividade, colaboração e aceitação
+                de críticas.
+              </p>
+              <p>Me destaco em várias ferramentas tecnológicas.</p>
+            </div>
+          </div>
 
-        <div className="flex w-full">
-      <div className="mx-auto w-full max-w-3xl rounded-2xl py-4">
-        <Disclosure>
-          {({ open }) => (
-            <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-neutral-200 px-4 py-2 text-left text-lg font-medium">
-                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
-                <ChevronUpIcon
-                  className={`${
-                    open ? 'rotate-180 transform' : ''
-                  } h-7 w-7`}
-                />
-              </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit eaque libero fugit molestias ipsa cum, vel voluptatibus ad esse soluta. Adipisci cumque beatae est similique doloribus excepturi? Odio, fugit id.
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-        <Disclosure as="div" className="mt-2">
-          {({ open }) => (
-            <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-neutral-200 px-4 py-2 text-left text-lg font-medium">
-                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
-                <ChevronUpIcon
-                  className={`${
-                    open ? 'rotate-180 transform' : ''
-                  } h-7 w-7`}
-                />
-              </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-xl">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio obcaecati sint perferendis eligendi, enim tempora voluptatum cupiditate pariatur qui voluptatibus saepe facilis asperiores fuga laboriosam voluptatem odio? Officia, quod nobis!
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-      </div>
-    </div>
+          <div className="flex w-full">
+            <div className="mx-auto w-full max-w-3xl rounded-2xl py-4">
+              <Disclosure>
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-neutral-200 px-4 py-2 text-left text-xl font-medium">
+                      <span>Formação</span>
+                      <ChevronUpIcon
+                        className={`${
+                          open ? "rotate-180 transform" : ""
+                        } h-7 w-7`}
+                      />
+                    </Disclosure.Button>
+                    <Disclosure.Panel className="px-4 pt-4 pb-2 text-xl">
+                      <ul className="flex list-disc flex-col pl-6">
+                        <li>Bacharel em Engenharia de Biossistemas - Universidade de São Paulo (2020 - 2025)</li>
+                        <li>Técnico em Informática - Escola Técnica Estadual de São Paulo (2017 - 2019)</li>
+                      </ul>
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+              <Disclosure as="div" className="mt-2">
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-neutral-200 px-4 py-2 text-left text-xl font-medium">
+                      <span>Experiência</span>
+                      <ChevronUpIcon
+                        className={`${
+                          open ? "rotate-180 transform" : ""
+                        } h-7 w-7`}
+                      />
+                    </Disclosure.Button>
+                    <Disclosure.Panel className="px-4 pt-4 pb-2 text-xl">
+                      <ul className="flex list-disc flex-col pl-6">
+                        <li>Gerente de Marketing - Biossistec Jr (jan 2022 - hoje)</li>
+                      </ul>
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+            </div>
+          </div>
 
-      </main>
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              <h2>Competências</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos unde earum praesentium facilis quibusdam recusandae corporis cupiditate voluptatibus a consectetur fuga accusamus quis reprehenderit, sunt aspernatur voluptas quasi architecto ab.
+              </p>
+            </div>
+            <div className={styles.card}>
+              <h2>Projetos</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos unde earum praesentium facilis quibusdam recusandae corporis cupiditate voluptatibus a consectetur fuga accusamus quis reprehenderit, sunt aspernatur voluptas quasi architecto ab.
+              </p>
+            </div>
+            <div className={styles.card}>
+              <h2>Contato</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos unde earum praesentium facilis quibusdam recusandae corporis cupiditate voluptatibus a consectetur fuga accusamus quis reprehenderit, sunt aspernatur voluptas quasi architecto ab.
+              </p>
+            </div>
+          </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://hmarrocos.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Distribuído por Marrocos, 2022
-          {/* <span className={styles.logo}>
+        </main>
+
+        <footer className={styles.footer}>
+          <a
+            href="https://hmarrocos.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Distribuído por Marrocos, 2022
+            {/* <span className={styles.logo}>
             <Image src="/favicon-16x16.png" alt="Marrocos Logo" width={16} height={16} />
           </span> */}
-        </a>
-      </footer>
-    </div>
+          </a>
+        </footer>
+      </div>
     </>
   );
 }
