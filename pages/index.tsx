6b6d2c46db769d3c1407bfcbf171f082cf665fd9
Marrocos <tendhendriw_94@hotmail.com>
2022-07-27 14:52:15 -0300

    proj

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,15 +35,36 @@ export default function Home(): JSX.Element {
                 , e amo o que faço.
               </p>
               <nav className={styles.buttons}>
-                <button className="whitespace-nowrap rounded-xl text-sky-600 px-4 py-2 font-semibold flex-1 border border-neutral-200" type="button">Sobre mim</button>
-                <button className="whitespace-nowrap rounded-xl text-sky-600 px-4 py-2 font-semibold flex-1 border border-neutral-200" type="button">Competências</button>
-                <button className="whitespace-nowrap rounded-xl text-sky-600 px-4 py-2 font-semibold flex-1 border border-neutral-200" type="button">Projetos</button>
-                <button className="whitespace-nowrap rounded-xl text-sky-600 px-4 py-2 font-semibold flex-1 border border-neutral-200" type="button">Contato</button>
+                <button
+                  className="whitespace-nowrap rounded-xl text-sky-600 px-4 py-2 font-semibold flex-1 border border-neutral-200"
+                  type="button"
+                >
+                  Sobre mim
+                </button>
+                <button
+                  className="whitespace-nowrap rounded-xl text-sky-600 px-4 py-2 font-semibold flex-1 border border-neutral-200"
+                  type="button"
+                >
+                  Competências
+                </button>
+                <button
+                  className="whitespace-nowrap rounded-xl text-sky-600 px-4 py-2 font-semibold flex-1 border border-neutral-200"
+                  type="button"
+                >
+                  Projetos
+                </button>
+                <button
+                  className="whitespace-nowrap rounded-xl text-sky-600 px-4 py-2 font-semibold flex-1 border border-neutral-200"
+                  type="button"
+                >
+                  Contato
+                </button>
               </nav>
             </div>
 
             <div className={styles.card}>
               <h2>Sobre mim</h2>
+              <hr className="placeholder-black" />
               <p>
                 Técnico em Informática e aluno de graduação, com habilidades em
                 resolução de problemas.
@@ -54,7 +75,7 @@ export default function Home(): JSX.Element {
               </p>
               <p>Me destaco em várias ferramentas tecnológicas.</p>
             </div>
-          </div>
+          
 
           <div className="flex w-full">
             <div className="mx-auto w-full max-w-3xl rounded-2xl py-4">
@@ -71,8 +92,14 @@ export default function Home(): JSX.Element {
                     </Disclosure.Button>
                     <Disclosure.Panel className="px-4 pt-4 pb-2 text-xl">
                       <ul className="flex list-disc flex-col pl-6">
-                        <li>Bacharel em Engenharia de Biossistemas - Universidade de São Paulo (2020 - 2025)</li>
-                        <li>Técnico em Informática - Escola Técnica Estadual de São Paulo (2017 - 2019)</li>
+                        <li>
+                          Bacharel em Engenharia de Biossistemas - Universidade
+                          de São Paulo (2020 - 2025)
+                        </li>
+                        <li>
+                          Técnico em Informática - Escola Técnica Estadual de
+                          São Paulo (2017 - 2019)
+                        </li>
                       </ul>
                     </Disclosure.Panel>
                   </>
@@ -91,7 +118,9 @@ export default function Home(): JSX.Element {
                     </Disclosure.Button>
                     <Disclosure.Panel className="px-4 pt-4 pb-2 text-xl">
                       <ul className="flex list-disc flex-col pl-6">
-                        <li>Gerente de Marketing - Biossistec Jr (jan 2022 - hoje)</li>
+                        <li>
+                          Gerente de Marketing - Biossistec Jr (jan 2022 - hoje)
+                        </li>
                       </ul>
                     </Disclosure.Panel>
                   </>
@@ -100,56 +129,115 @@ export default function Home(): JSX.Element {
             </div>
           </div>
 
-          <div className={styles.grid}>
+          
             <div className={styles.card}>
               <h2>Competências</h2>
+              <hr className="placeholder-black" />
               <div className="flex flex-col gap-2">
                 <div className="flex gap-2">
-                  <img className="flex self-center h-4" src="/nextjs.png" alt="Next.js"/>
-                  Next.js</div>
+                  <img
+                    className="flex self-center h-4"
+                    src="/nextjs.png"
+                    alt="Next.js"
+                  />
+                  Next.js
+                </div>
                 <div className="flex gap-2">
-                  <img className="self-center h-4" src="/reactjs.png" alt="React.js"/>
-                  React.js</div>
+                  <img
+                    className="self-center h-4"
+                    src="/reactjs.png"
+                    alt="React.js"
+                  />
+                  React.js
+                </div>
                 <div className="flex gap-2">
-                  <img className="flex self-center h-4" src="/ts.png" alt="TypeScript"/>
-                  TypeScript</div>
+                  <img
+                    className="flex self-center h-4"
+                    src="/ts.png"
+                    alt="TypeScript"
+                  />
+                  TypeScript
+                </div>
                 <div className="flex gap-2">
-                  <img className="flex self-center h-4" src="/esl.png" alt="ESLint"/>
-                  ESLint</div>
+                  <img
+                    className="flex self-center h-4"
+                    src="/esl.png"
+                    alt="ESLint"
+                  />
+                  ESLint
+                </div>
                 <div className="flex gap-2">
-                  <img className="flex self-center h-4" src="/twcss.png" alt="Tailwind CSS"/>
-                  Tailwind CSS</div>
+                  <img
+                    className="flex self-center h-4"
+                    src="/twcss.png"
+                    alt="Tailwind CSS"
+                  />
+                  Tailwind CSS
+                </div>
                 <div className="flex gap-2">
-                  <img className="flex self-center h-4" src="/sass.png" alt="SASS/SCSS"/>
-                  SASS/SCSS</div>
+                  <img
+                    className="flex self-center h-4"
+                    src="/sass.png"
+                    alt="SASS/SCSS"
+                  />
+                  SASS/SCSS
+                </div>
                 <div className="flex gap-2">
-                  <img className="flex self-center h-4" src="/seo.png" alt="SEO"/>
-                  Otimização para mecanismos de busca (SEO)</div>
+                  <img
+                    className="flex self-center h-4"
+                    src="/seo.png"
+                    alt="SEO"
+                  />
+                  Otimização para mecanismos de busca (SEO)
+                </div>
                 <div className="flex gap-2">
-                  <img className="flex self-center h-4" src="/mdx.png" alt="MDX"/>
-                  MultiDimensional eXpressions</div>
+                  <img
+                    className="flex self-center h-4"
+                    src="/mdx.png"
+                    alt="MDX"
+                  />
+                  MultiDimensional eXpressions
+                </div>
                 <div className="flex gap-2">
-                  <img className="flex self-center h-4" src="/ga.png" alt="Google Analytics"/>
-                  Google Analytics</div>
+                  <img
+                    className="flex self-center h-4"
+                    src="/ga.png"
+                    alt="Google Analytics"
+                  />
+                  Google Analytics
+                </div>
                 <div className="flex gap-2">
-                  <img className="flex self-center h-4" src="/git.png" alt="Git"/>
-                  Git</div>
+                  <img
+                    className="flex self-center h-4"
+                    src="/git.png"
+                    alt="Git"
+                  />
+                  Git
+                </div>
               </div>
             </div>
             <div className={styles.card}>
               <h2>Projetos</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos unde earum praesentium facilis quibusdam recusandae corporis cupiditate voluptatibus a consectetur fuga accusamus quis reprehenderit, sunt aspernatur voluptas quasi architecto ab.
-              </p>
+              <hr className="placeholder-black" />
+                <div className="flex gap-4 rounded-xl border relative h-20 cursor-pointer overflow-hidden px-0 py-0">
+                  <img className="h-20 w-20 rounded-xl" src="" alt=""></img>
+                  <div className="flex flex-1 flex-col justify-between overflow-hidden py-2 pr-4">
+                    <div className="flex items-center justify-between gap-2">
+                      <h4 className="text-lg font-medium flex-1 line-clamp-1">
+                        Teste
+                      </h4>
+                    </div>
+                    <p className="text-lg font-light overflow-hidden text-ellipsis whitespace-nowrap">
+                      Teste
+                    </p>
+                  </div>
+                </div>
             </div>
             <div className={styles.card}>
               <h2>Contato</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos unde earum praesentium facilis quibusdam recusandae corporis cupiditate voluptatibus a consectetur fuga accusamus quis reprehenderit, sunt aspernatur voluptas quasi architecto ab.
-              </p>
+              <hr className="placeholder-black" />
             </div>
           </div>
-
         </main>
 
         <footer className={styles.footer}>
